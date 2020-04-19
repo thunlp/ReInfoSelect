b6ecfeb4665468a039390f074961dccf877c983e
zkt12 <347925985@qq.com>
2020-04-19 11:50:12 +0800

    update

--- ReInfoSelect/inference/inference.sh
+++ ReInfoSelect/inference/inference.sh
@@ -1,5 +1,6 @@
 CUDA_VISIBLE_DEVICES=0 python cknrm_inference.py \
---test_file ./bm25.jsonl \
---out_path ./cknrm.jsonl \
---pretrained_model ./reinfoselect_cknrm_covid19 \
---embedding_path ./glove.6B.300d.txt
+--test_file ./dlg_bm25.jsonl \
+--out_path ./dlg_marco_recknrm.jsonl \
+--pretrained_model ./dlg_marco_recknrm_128 \
+--embedding_path ./glove.6B.300d.txt \
+--max_doc_len 128
