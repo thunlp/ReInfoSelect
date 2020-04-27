540f6f05de363457ee4afff673652f4e98b6a397
zkt12 <347925985@qq.com>
2020-04-27 23:57:30 +0800

    update

--- ReInfoSelect/gen_trec.py
+++ ReInfoSelect/gen_trec.py
@@ -1,13 +1,14 @@
+import argparse
 import json
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-dev', type=str, default='../data/dev_toy.tsv')
-    parser.add_argument('-res', type=str, default='../results/cknrm.trec')
+    parser.add_argument('-dev', type=str, default='./data/ClueWeb09/dev.tsv')
+    parser.add_argument('-res', type=str, default='./results/cknrm.trec')
     args = parser.parse_args()
 
     score_dic = {}
-    with open('f1.score', 'r') as r:
+    with open('../results/f1.score', 'r') as r:
         for line in r:
             line = line.strip('\n').split('\t')
             score_dic[line[0] + '$' + line[1]] = line[2]
@@ -34,3 +35,6 @@ def main():
                 ps[out[0]] = 1
                 f.write(' '.join([qid, 'Q0', out[0], str(len(ps)), str(out[1]), 'default']) + '\n')
     f.close()
+
+if __name__ == "__main__":
+    main()
