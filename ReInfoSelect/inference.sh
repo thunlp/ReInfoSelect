11156d8d4e6e8d5b1e63719caf891db561d53c1a
zkt12 <347925985@qq.com>
2020-05-08 11:13:18 +0800

    update sh files

--- ReInfoSelect/inference.sh
+++ ReInfoSelect/inference.sh
@@ -1,16 +1,17 @@
 CUDA_VISIBLE_DEVICES=0 \
 python main.py \
         -mode infer \
-        -model cknrm \
+        -model bert \
         -max_input 1280000 \
-        -checkpoint ../checkpoints/reinfoselect_cknrm.bin \
+        -pretrain allenai/scibert_scivocab_uncased \
+        -checkpoint ../checkpoints/reinfoselect_bert.bin \
         -dev ../data/dev_toy.tsv \
         -embed ../data/glove.6B.300d.txt \
         -vocab_size 400002 \
         -embed_dim 300 \
-        -res_trec ../results/cknrm.trec \
-        -res_json ../results/cknrm.json \
-        -res_feature ../features/cknrm_features \
+        -res_trec ../results/bert.trec \
+        -res_json ../results/bert.json \
+        -res_feature ../features/bert_features \
         -gamma 0.99 \
         -T 1 \
         -n_kernels 21 \
